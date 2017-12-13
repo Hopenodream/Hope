08be816ed51d98e2cb1b87eff78ad21bd35596ef
Hopenodream <34496242+Hopenodream@users.noreply.github.com>
2017-12-13 11:25:25 -0500

    Update operations.py

--- operations.py
+++ operations.py
@@ -4,24 +4,24 @@ from functions import *
 
 def arithmetic_summation():
     print('for the following value inputs, leave blank if the value is unknown')
-    asub1 = (input('whats the first value in the sequence>>>'))
-    asubn = (input('whats the value of A given a value of n>>>'))
-    index = (input('value of n for A sub n>>>'))
+    # these values should be stored in the global variable
+    aSub1 = (input('whats the first value in the sequence>>>'))
+    aSubN = (input('whats the value of A given a value of n>>>'))
+    n = (input('value of n for A sub n>>>'))
 
-    if not asub1:
+    if not aSub1:
         values = list(a_sub_1_missing())
-        ASub1 = find_a_1(values[0], values[1], values[2])
+        aSub1 = find_a_1(values[0], values[1], values[2])
         pass
-    elif not asubn:
+    if not aSubN:
         values = list(a_sub_n_missing())
-        ASubN = find_a_sub_n(values[0], values[1], values[2])
+        aSubN = find_a_sub_n(values[0], values[1], values[2])
         pass
-    elif not index:
+    if not n:
         values = list(n_missing())
         n = find_n(values[0], values[1], values[2])
         pass
-    sSubN = (n * (ASub1 + ASubN)) / 2
-    print('the answer is ', sSubN)
+    sSubN = (n * (aSub1 + aSubN)) / 2
 
 
 pass
@@ -29,25 +29,20 @@ pass
 
 def arithmetic_sequence():
     print('for the following value inputs, leave blank if the value is unknown')
-    asub1 = (input('first value in sequence >>>'))
-    commond = (input('common difference >>>'))
-    index = (input('index of the number you\'re trying to find in the sequence'))
+    aSub1 = (input('first value in sequence >>>'))
+    d = (input('common difference >>>'))
+    n = (input('index of the number you\'re trying to find in the sequence'))
 
     if not asub1:
-        print('=====finding a sub 1=====')
         values = list(a_sub_1_missing())
-        ASub1 = find_a_1(values[0], values[1], values[2])
+        aSub1 = find_a_1(values[0], values[1], values[2])
         pass
-    elif not commond:
-        print('=====finding the common difference=====')
+    if not d:
         values = list(d_missing())
-        asubx, x, asuby, y = values[0], values[1], values[2], values[3]
-        d = float(find_common_difference(asubx, x, asuby, y))
+        d = float(find_common_difference(values[0], values[1], values[2], values[3]))
         pass
-    elif not index:
-        print('=====finding the nth index=====')
+    if not n:
         values = list(n_missing())
         n = float(find_n(values[0], values[1], values[2]))
         pass
-    asubn = find_a_sub_n(float(d), float(asub1), int(n))
-    print('a sub n ', asubn)
+    aSubN = find_a_sub_n(float(d), float(asub1), int(n))
