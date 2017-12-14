a8f832a8a0f3e788f9106ebf061d9d38cab3d11f
Hope <deathbethecat@gmail.com>
2017-12-13 22:46:54 -0500

    beta 0.2 changelog -updated all functions to use proper variables -added float() and int() in places where they needed to be

--- functions.py
+++ functions.py
@@ -4,30 +4,41 @@ import var
 
 def operation_selection(prompt='choose the operation you would like to perform:'):
     print(var.operations)
-    selection = input(prompt)
-    print('you have chosen: ', selection)
-    print('                ==============================         ')
-    if selection == var.operations[0]:
+    var.selection = input(prompt)
+    print('you have chosen: ', var.selection)
+    print('          ==============================         ')
+    if var.selection == var.operations[0]:
         arithmetic_summation()
         pass
-    elif selection == var.operations[1]:
+    elif var.selection == var.operations[1]:
         arithmetic_sequence()
         pass
     else:
         print('unrecognized selection, try again')
 
 
-def return_answers(operation):
-    print('=======', operation, '=======')
-    if operation == var.operations[0] or var.operations[1]:
-        print('common difference ', d)
-        print('index "n" ', var.n)
-        print('a sub 1 ', var.aSub1)
-        if operation == var.operations[1]:
-            print('a sub n ', var.aSubN)  # prints a sub n if arithmetic sequence was chosen
-            pass
-        if operation == var.operations[0]:
-            print('s sub n ', var.sSubN)  # prints partial sum if arithmetic summation was chosen
-            pass
+def return_answers():
+    print('=======', var.selection, '=======')
+    if var.selection == var.operations[0] or var.operations[1]:  # if selection is arithmetic
+        if var.d:
+            print('common difference :', var.d)
+        if var.n:
+            print('index "n" :', var.n)
+        if var.aSub1:
+            print('a sub 1 : ', var.aSub1)
+        if var.selection == var.operations[1]:
+            if var.aSubN:
+                print('a sub ', var.n, ': ', var.aSubN)  # prints a sub n if arithmetic sequence was chosen
+                pass
+        if var.selection == var.operations[0]:
+            if var.sSubN:
+                print('s sub ', var.n, ': ', var.sSubN)  # prints partial sum if arithmetic summation was chosen
+                pass
         pass
-    pass
+
+
+def init():
+    print('\nWelcome to Zaxy - an advanced mathematical calculator')
+    print('\n')
+    print('                                           Created by Hope - Beta v0.2')
+    print('                    ==================Enjoy==================')
