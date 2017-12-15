98eba98ecbf8d88eae2e4697dd505d3b336ea879
Hopenodream <34496242+Hopenodream@users.noreply.github.com>
2017-12-15 12:07:20 -0500

    synchronized function names, cleaned up code

--- operations.py
+++ operations.py
@@ -5,22 +5,17 @@ from functions import *
 def arithmetic_summation():
     print('for the following value inputs, leave blank if the value is unknown')
     # these values should be stored in the global variable
-    var.aSub1 = float(input('whats the first value in the sequence>>>'))
-    var.aSubN = float(input('whats the value of A given an index of n>>>'))
-    var.n = int(input('value of n for A sub n>>>'))
+    s_sub_n_missing()
 
     if not var.aSub1:
         a_sub_1_missing()
-        find_a_1()
-        pass
+        find_a_sub_1()
     if not var.aSubN:
         a_sub_n_missing()
         find_a_sub_n()
-        pass
     if not var.n:
-        n_missing()
-        find_n())
-        pass
+        index_missing()
+        find_index())
     find s_sub_n)()
 
 
@@ -29,22 +24,17 @@ pass
 
 def arithmetic_sequence():
     print('for the following value inputs, leave blank if the value is unknown')
-    var.aSub1 = float(input('first value in sequence>>>'))
-    var.d = float(input('common difference>>>'))
-    var.n = int(input('index of the number you\'re trying to find in the sequence'))
-
+    
+    a_sub_n_missing()
     if not var.aSub1:
         a_sub_1_missing()
-        find_a_1()
-        pass
+        find_a_sub_1()
     if not var.d:
-        change_missing()
+        common_change_missing()
         find_common_change()
-        pass
     if not var.n:
-        n_missing()
-        find_n()
-        pass
+        index_missing()
+        find_index()
     find_a_sub_n()
     
 pass
@@ -52,20 +42,17 @@ pass
 
 def geometric_sequence():
     print('for the following value inputs, leave blank if the value is unknown')
-    var.aSub1 = float(input('first value in sequence>>>'))
-    var.r = float(input('common rate>>>'))
-    var.n = int(input('index'))
     
+    a_sub_n_missing()
     if not var.aSub1:
         a_sub_1_missing()
-        find_a_1()
+        find_a_sub_1()
     if not var.r:
-        change_missing()
+        common_change_missing()
         find_common_change()
     if not var.n:
-        n_missing()
-        find_n()
-        pass
+        index_missing()
+        find_index()
     find_a_sub_n()
     
 pass
@@ -74,3 +61,14 @@ pass
 def geometric_summation():
     print('for the following value inputs, leave blank if the value is unknown')
     
+    s_sub_n_missing()
+    if not var.aSub1:
+        a_sub_1_missing()
+        find_a_sub_1()
+    if not var.r:
+        common_change_missing()
+        find_common_change()
+    if not var.n:
+        index_missing()
+        find_index()
+    find_s_sub_n()
