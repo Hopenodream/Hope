44d1757e172f5470a6db51417e48b809ecf6f19c
Hopenodream <34496242+Hopenodream@users.noreply.github.com>
2017-12-15 10:43:41 -0500

    removed perimeters in finding functions

--- operations.py
+++ operations.py
@@ -5,23 +5,23 @@ from functions import *
 def arithmetic_summation():
     print('for the following value inputs, leave blank if the value is unknown')
     # these values should be stored in the global variable
-    var.aSub1 = (input('whats the first value in the sequence>>>'))
-    var.aSubN = (input('whats the value of A given an index of n>>>'))
-    var.n = (input('value of n for A sub n>>>'))
+    var.aSub1 = float(input('whats the first value in the sequence>>>'))
+    var.aSubN = float(input('whats the value of A given an index of n>>>'))
+    var.n = int(input('value of n for A sub n>>>'))
 
     if not var.aSub1:
         a_sub_1_missing()
-        find_a_1(float(var.d), float(var.aSubX), int(var.x))
+        find_a_1()
         pass
     if not var.aSubN:
         a_sub_n_missing()
-        find_a_sub_n(var.d, var.aSub1, var.n)
+        find_a_sub_n()
         pass
     if not var.n:
         n_missing()
-        find_n(int(var.n), float(var.aSub1), float(var.aSubN))
+        find_n())
         pass
-    var.sSubN = (int(var.n) * (float(var.aSub1) + float(var.aSubN))) / 2
+    find s_sub_n)()
 
 
 pass
@@ -29,20 +29,48 @@ pass
 
 def arithmetic_sequence():
     print('for the following value inputs, leave blank if the value is unknown')
-    var.aSub1 = (input('first value in sequence >>>'))
-    var.d = (input('common difference >>>'))
-    var.n = (input('index of the number you\'re trying to find in the sequence'))
+    var.aSub1 = float(input('first value in sequence>>>'))
+    var.d = float(input('common difference>>>'))
+    var.n = int(input('index of the number you\'re trying to find in the sequence'))
 
     if not var.aSub1:
         a_sub_1_missing()
-        find_a_1(float(var.d), float(var.aSubX), int(var.x))
+        find_a_1()
         pass
     if not var.d:
-        d_missing()
-        find_common_difference(float(var.aSubX), int(var.x), float(var.aSubY), int(var.y))
+        change_missing()
+        find_common_change()
         pass
     if not var.n:
         n_missing()
-        find_n(int(var.n), float(var.aSub1), float(var.aSubN))
+        find_n()
         pass
-    find_a_sub_n(float(var.d), float(var.aSub1), int(var.n))
+    find_a_sub_n()
+    
+pass
+
+
+def geometric_sequence():
+    print('for the following value inputs, leave blank if the value is unknown')
+    var.aSub1 = float(input('first value in sequence>>>'))
+    var.r = float(input('common rate>>>'))
+    var.n = int(input('index'))
+    
+    if not var.aSub1:
+        a_sub_1_missing()
+        find_a_1()
+    if not var.r:
+        change_missing()
+        find_common_change()
+    if not var.n:
+        n_missing()
+        find_n()
+        pass
+    find_a_sub_n()
+    
+pass
+
+
+def geometric_summation():
+    print('for the following value inputs, leave blank if the value is unknown')
+    
