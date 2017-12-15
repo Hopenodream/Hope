fcc24a8d75e9bcd05b202cad61ae198502ac25cf
Hopenodream <34496242+Hopenodream@users.noreply.github.com>
2017-12-15 12:13:24 -0500

    created docstring, commented on variables

--- var.py
+++ var.py
@@ -1,15 +1,16 @@
-#  stores all global variables
+"""
+Stores all global variables
+"""
 
-
-global d, aSub1, aSubN, aSubX, aSubY, x, y, sSubN, n, operations, selection
-d = None
-aSub1 = None
-aSubN = None
-aSubX = None
-aSubY = None
-x = None
-y = None
-sSubN = None
-n = None
+global d, aSub1, aSubN, aSubX, aSubY, x, y, sSubN, n, operations, selection  # declares all variables global
+d = None      # common difference
+aSub1 = None  # first number in sequence 
+aSubN = None  # number in sequence given index n
+aSubX = None  # number in sequence with index x
+aSubY = None  # number in sequence with index y
+x = None      # index x, correlates with aSubX
+y = None      # index y, correlates with aSUbY
+sSubN = None  # partial sum given index n
+n = None      # index n
 operations = ['arithmetic summation', 'arithmetic sequence', 'geometric summation', 'geometric sequence']
 selection = None
